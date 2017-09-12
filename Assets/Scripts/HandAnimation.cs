36b8f45bd87e6e94f15bd8e10445df32f3c768e1
Marc <marc_2772@hotmail.com>
2017-09-11 21:27:49 -0400

    Added the hands + animation

--- Assets/Scripts/HandAnimation.cs
+++ Assets/Scripts/HandAnimation.cs
@@ -3,37 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 
-public class HandAnimation : MonoBehaviour {
-    public bool isRightHand = false;
+public class HandAnimation : MonoBehaviour
+{
+	public SteamVR_TrackedController controller;
 
-    SteamVR_Controller.Device controller;
+	private Animator animator;
 
-    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
-    private Valve.VR.EVRButtonId trigger = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
-    private Animator animator;
-    // Use this for initialization
-    void Start () {
-        animator = GetComponent<Animator>();
+	void OnEnable()
+	{
+		controller.TriggerClicked += CloseHand;
+		controller.TriggerUnclicked += OpenHand;
+	}
 
-        if (isRightHand)
-        {
-            controller = SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost));
-        }
-        else
-        {
-            controller = SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost));
-        }
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        if (controller.GetPress(trigger))
-        {
-            animator.SetBool("close", true);
-        }
+	void OnDisable()
+	{
+		controller.TriggerClicked -= CloseHand;
+		controller.TriggerUnclicked -= OpenHand;
+	}
 
-        if (controller.GetPressUp(trigger)) {
-            animator.SetBool("close", false);
-        }
-    }
+	void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	void CloseHand(object sender, ClickedEventArgs e)
+	{
+		animator.SetBool("close", true);
+	}
+
+	void OpenHand(object sender, ClickedEventArgs e)
+	{
+		animator.SetBool("close", false);
+	}
 }
