822b46de189bcd934e4b2e457169e4ca58938a61
OlivierB11 <31495911+OlivierB11@users.noreply.github.com>
2017-09-15 01:01:04 -0400

    Added grabbing system to hand Interaction. New script grabbable to make an object grabbable. Added colliders to hands.

---
+++ Assets/Grabbable.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Grabbable : MonoBehaviour {
+    public HandInteraction rightHand;
+    public HandInteraction leftHand;
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "RightFingers")
+        {
+            rightHand.ObjectColliding = gameObject;
+        }
+        else if (other.tag == "LeftFingers") {
+            leftHand.ObjectColliding = gameObject;
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "RightFingers")
+        {
+            rightHand.ObjectColliding = gameObject;
+        }
+        else if (other.tag == "LeftFingers")
+        {
+            leftHand.ObjectColliding = gameObject;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "RightFingers")
+        {
+            if (rightHand.ObjectColliding == gameObject)
+            {
+                rightHand.ObjectColliding = null;
+            }
+        }
+        else if (other.tag == "LeftFingers")
+        {
+            if (leftHand.ObjectColliding == gameObject) {
+                leftHand.ObjectColliding = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HandInteraction.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class HandInteraction : MonoBehaviour
+{
+	public SteamVR_TrackedController controller;
+    public float velocityMultiplier = 1f;
+
+    private GameObject objectInHand;
+    private GameObject objectColliding;
+
+    private Rigidbody rb;
+
+	private Animator animator;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)controller.controllerIndex); }
+    }
+
+    void OnEnable()
+	{
+		controller.TriggerClicked += CloseHand;
+		controller.TriggerUnclicked += OpenHand;
+        controller.TriggerClicked += Grab;
+        controller.TriggerUnclicked += ReleaseObject;
+	}
+
+	void OnDisable()
+	{
+		controller.TriggerClicked -= CloseHand;
+		controller.TriggerUnclicked -= OpenHand;
+        controller.TriggerClicked -= Grab;
+        controller.TriggerUnclicked -= ReleaseObject;
+    }
+
+	void Awake()
+	{
+		animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+	}
+
+	void CloseHand(object sender, ClickedEventArgs e)
+	{
+		animator.SetBool("close", true);
+	}
+
+	void OpenHand(object sender, ClickedEventArgs e)
+	{
+		animator.SetBool("close", false);
+	}
+
+    public void Grab(object sender, ClickedEventArgs e)
+    {
+        if (!objectInHand && objectColliding)
+        {
+            objectInHand = objectColliding;
+            objectColliding = null;
+
+            var joint = AddFixedJoint();
+            joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+        }
+    }
+
+    private void ReleaseObject(object sender, ClickedEventArgs e)
+    {
+        if (objectInHand)
+        {
+            if (GetComponent<FixedJoint>())
+            {
+                GetComponent<FixedJoint>().connectedBody = null;
+                Destroy(GetComponent<FixedJoint>());
+
+                objectInHand.GetComponent<Rigidbody>().velocity = velocityMultiplier * Controller.velocity;
+                objectInHand.GetComponent<Rigidbody>().angularVelocity = velocityMultiplier * Controller.angularVelocity;
+            }
+            objectInHand = null;
+        }
+    }
+
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fixedJoint = gameObject.AddComponent<FixedJoint>();
+        fixedJoint.breakForce = 20000;
+        fixedJoint.breakTorque = 20000;
+        return fixedJoint;
+    }
+    
+    public GameObject ObjectColliding
+    {
+
+        get
+        {
+            return objectColliding;
+        }
+
+        set
+        {
+            if (value == null) {
+                objectColliding = null;
+                return;
+            }
+
+            if (value.GetComponent<Rigidbody>())
+            {
+                objectColliding = value;
+            }
+        }
+    }
+}
