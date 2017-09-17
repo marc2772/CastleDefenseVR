2beb100ceb78dc223e7ae41e757678102530c48a
Marc <marc_2772@hotmail.com>
2017-09-17 13:06:43 -0400

    Added basic pathfinding with a test ennemy

---
+++ Assets/Scripts/Killable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killable : MonoBehaviour
+{
+	public int cashDrop = 0;
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+
+	}
+}
---
+++ Assets/Scripts/PathFind.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathFind : MonoBehaviour
+{
+	public Transform followedEntity;
+
+	private NavMeshAgent agent;
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update()
+	{
+		agent.destination = followedEntity.position;
+	}
+}
