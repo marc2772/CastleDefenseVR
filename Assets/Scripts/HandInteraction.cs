cc871b506600ab33d48564835a82bc672bc7ac42
OlivierB11 <31495911+OlivierB11@users.noreply.github.com>
2017-09-21 02:06:14 -0400

    Added hand animations. Grip close the middle,ring and pinky. Trigger close index. Thumbstick/Pad bend thumb.

--- Assets/Scripts/HandInteraction.cs
+++ Assets/Scripts/HandInteraction.cs
@@ -21,18 +21,30 @@ public class HandInteraction : MonoBehaviour
 
     void OnEnable()
 	{
-		controller.Gripped += CloseHand;
-		controller.Ungripped += OpenHand;
+		controller.Gripped += CloseThreeFingers;
+		controller.Ungripped += OpenThreeFingers;
         controller.Gripped += Grab;
         controller.Ungripped += ReleaseObject;
+
+        controller.TriggerClicked += CloseIndex;
+        controller.TriggerUnclicked += OpenIndex;
+
+        controller.PadTouched += CloseThumb;
+        controller.PadUntouched += OpenThumb;
 	}
 
 	void OnDisable()
 	{
-		controller.Gripped -= CloseHand;
-		controller.Ungripped -= OpenHand;
+		controller.Gripped -= CloseThreeFingers;
+		controller.Ungripped -= OpenThreeFingers;
         controller.Gripped -= Grab;
         controller.Ungripped -= ReleaseObject;
+
+        controller.TriggerClicked -= CloseIndex;
+        controller.TriggerUnclicked -= OpenIndex;
+
+        controller.PadTouched += CloseThumb;
+        controller.PadUntouched += OpenThumb;
     }
 
 	void Awake()
@@ -54,6 +66,37 @@ public class HandInteraction : MonoBehaviour
 	{
 		animator.SetBool("close", false);
 	}
+
+    void CloseThreeFingers(object sender, ClickedEventArgs e)
+    {
+        Debug.Log("hello");
+        animator.SetBool("three_fingers_close", true);
+    }
+
+    void OpenThreeFingers(object sender, ClickedEventArgs e)
+    {
+        animator.SetBool("three_fingers_close", false);
+    }
+
+    void CloseIndex(object sender, ClickedEventArgs e)
+    {
+        animator.SetBool("close_index", true);
+    }
+
+    void OpenIndex(object sender, ClickedEventArgs e)
+    {
+        animator.SetBool("close_index", false);
+    }
+
+    void CloseThumb(object sender, ClickedEventArgs e)
+    {
+        animator.SetBool("close_thumb", true);
+    }
+
+    void OpenThumb(object sender, ClickedEventArgs e)
+    {
+        animator.SetBool("close_thumb", false);
+    }
 
     public void Grab(object sender, ClickedEventArgs e)
     {
