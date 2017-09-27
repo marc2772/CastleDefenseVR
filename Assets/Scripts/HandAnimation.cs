6f7b91d2b3a11be3354e49463703e877fa6f17e7
OlivierB11 <31495911+OlivierB11@users.noreply.github.com>
2017-09-26 23:43:45 -0400

    Revert "Merge branch 'HandInteraction'"
    This reverts commit 94b7eb4aca45d6ce99cacf00bf8f5f26b8a512b5, reversing
    changes made to 9d0a15039b73da04f4e247553ed8d49879e16a1d.
    
    # Conflicts:
    #	Assets/Scenes/Prototype.unity

--- Assets/Scripts/HandAnimation.cs
+++ Assets/Scripts/HandAnimation.cs
@@ -1,101 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
-public class HandAnimation : MonoBehaviour {
+public class HandAnimation : MonoBehaviour
+{
+	public SteamVR_TrackedController controller;
 
-    public SteamVR_TrackedController controller;
+	private Animator animator;
 
-    private Animator animator;
-    private SkinnedMeshRenderer skinnedMeshRenderer;
+	void OnEnable()
+	{
+		controller.TriggerClicked += CloseHand;
+		controller.TriggerUnclicked += OpenHand;
+	}
 
-    void Awake()
-    {
-        animator = GetComponent<Animator>();
-        skinnedMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
-    }
+	void OnDisable()
+	{
+		controller.TriggerClicked -= CloseHand;
+		controller.TriggerUnclicked -= OpenHand;
+	}
 
-    void OnEnable()
-    {
-        controller.Gripped += CloseThreeFingers;
-        controller.Ungripped += OpenThreeFingers;
+	void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
 
-        controller.TriggerClicked += CloseIndex;
-        controller.TriggerUnclicked += OpenIndex;
+	void CloseHand(object sender, ClickedEventArgs e)
+	{
+		animator.SetBool("close", true);
+	}
 
-        controller.PadTouched += CloseThumb;
-        controller.PadUntouched += OpenThumb;
-    }
-
-    void OnDisable()
-    {
-        controller.Gripped -= CloseThreeFingers;
-        controller.Ungripped -= OpenThreeFingers;
-
-        controller.TriggerClicked -= CloseIndex;
-        controller.TriggerUnclicked -= OpenIndex;
-
-        controller.PadTouched += CloseThumb;
-        controller.PadUntouched += OpenThumb;
-    }
-
-    public void CloseHand()
-    {
-        animator.SetBool("close", true);
-    }
-
-    public void OpenHand()
-    {
-        animator.SetBool("close", false);
-    }
-
-    void CloseHand(object sender, ClickedEventArgs e)
-    {
-        animator.SetBool("close", true);
-    }
-
-    void OpenHand(object sender, ClickedEventArgs e)
-    {
-        animator.SetBool("close", false);
-    }
-
-    void CloseThreeFingers(object sender, ClickedEventArgs e)
-    {
-        animator.SetBool("three_fingers_close", true);
-    }
-
-    void OpenThreeFingers(object sender, ClickedEventArgs e)
-    {
-        animator.SetBool("three_fingers_close", false);
-    }
-
-    void CloseIndex(object sender, ClickedEventArgs e)
-    {
-        animator.SetBool("close_index", true);
-    }
-
-    void OpenIndex(object sender, ClickedEventArgs e)
-    {
-        animator.SetBool("close_index", false);
-    }
-
-    void CloseThumb(object sender, ClickedEventArgs e)
-    {
-        animator.SetBool("close_thumb", true);
-    }
-
-    void OpenThumb(object sender, ClickedEventArgs e)
-    {
-        animator.SetBool("close_thumb", false);
-    }
-
-    public void HideHand()
-    {
-        skinnedMeshRenderer.enabled = false;
-    }
-
-    public void ShowHand()
-    {
-        skinnedMeshRenderer.enabled = true;
-    }
+	void OpenHand(object sender, ClickedEventArgs e)
+	{
+		animator.SetBool("close", false);
+	}
 }
