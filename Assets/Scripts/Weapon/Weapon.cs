65434c9bac66638bb76539f571324fb2d049b9ae
Marc <marc_2772@hotmail.com>
2017-10-02 21:13:33 -0400

    Added dmg and hp values + drop popup

---
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+	public int damageValue = 1;
+
+	public int GetDamageValue()
+	{
+		return damageValue;
+	}
+}
